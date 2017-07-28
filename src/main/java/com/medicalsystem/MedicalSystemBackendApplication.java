c7ac0f222436dd4d7e249d954309c0aea12acc6a
sienia0@gmail.com <sienia0@gmail.com>
2017-07-28 15:38:22 +0200

    Set up project v2

---
+++ src/main/java/com/medicalsystem/MedicalSystemBackendApplication.java
@@ -0,0 +1,12 @@
+package com.medicalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalSystemBackendApplication.class, args);
+	}
+}
