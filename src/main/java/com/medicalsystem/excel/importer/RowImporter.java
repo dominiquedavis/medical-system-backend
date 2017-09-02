038d60f2b5aa890455f422e116a7e27dff0e6ab1
kamildev7 <k.93kamil@gmail.com>
2017-09-02 09:02:14 +0200

    Change RowImporter

--- src/main/java/com/medicalsystem/excel/importer/RowImporter.java
+++ src/main/java/com/medicalsystem/excel/importer/RowImporter.java
@@ -1,19 +1,19 @@
 package com.medicalsystem.excel.importer;
 
 import com.medicalsystem.model.field.Field;
-import com.medicalsystem.model.value.DoubleFieldValue;
-import com.medicalsystem.model.value.FieldValue;
-import com.medicalsystem.model.value.TextFieldValue;
+import com.medicalsystem.model.value.*;
 import com.medicalsystem.service.FieldService;
 import com.medicalsystem.service.FieldValueService;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import org.apache.poi.ss.usermodel.Row;
 
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -32,15 +32,26 @@ public class RowImporter {
         cellIterator.next();
         while (cellIterator.hasNext()) {
             Cell cell = cellIterator.next();
+            switch (cell.getCellType()) {
+                case Cell.CELL_TYPE_STRING:
+                    saveStringValue(cell, patientId, columnIndexes);
+                    break;
+                case Cell.CELL_TYPE_NUMERIC:
+                    saveNumericValue(cell, patientId, columnIndexes);
+                    break;
+            }
             if (cell.getCellType() == 0) {
                 saveNumericValue(cell, patientId, columnIndexes);
             } else {
-                saveStringValue(cell, patientId, columnIndexes);
             }
         }
 
         log.info("ROW PERSISTED: " + row.getRowNum());
     }
+
+
+
+    //TO-DO remove the repeating code
 
     private void saveStringValue(Cell cell, int patientId, Map<Integer, String> columnIndexes) {
         FieldValue<String> field = new TextFieldValue();
@@ -55,6 +66,46 @@ public class RowImporter {
     }
 
     private void saveNumericValue(Cell cell, int patientId, Map<Integer, String> columnIndexes) {
+
+        if (DateUtil.isCellDateFormatted(cell)) {
+            saveDateValue(cell, patientId, columnIndexes);
+        } else {
+            Double numericCellValue = cell.getNumericCellValue();
+            if(Math.floor(numericCellValue) == numericCellValue){
+                saveIntegerValue(cell, patientId, columnIndexes);
+            } else {
+                saveDoubleValue(cell, patientId, columnIndexes);
+            }
+        }
+    }
+
+    private void saveIntegerValue(Cell cell, int patientId, Map<Integer, String> columnIndexes) {
+        FieldValue<Integer> field = new IntegerFieldValue();
+        field.setPatientId(patientId);
+        Field fieldByName = fieldService.findByName(columnIndexes.get(cell.getColumnIndex()));
+        if (fieldByName == null) {
+            return;
+        }
+        field.setField(fieldByName);
+        field.setValue((int) cell.getNumericCellValue());
+        fieldValueService.saveOrUpdate(field);
+    }
+
+    //TO-DO create a date parser
+    private void saveDateValue(Cell cell, int patientId, Map<Integer, String> columnIndexes) {
+        FieldValue<Date> field = new DateFieldValue();
+        field.setPatientId(patientId);
+        Field fieldByName = fieldService.findByName(columnIndexes.get(cell.getColumnIndex()));
+        if (fieldByName == null) {
+            return;
+        }
+        field.setField(fieldByName);
+        field.setValue(cell.getDateCellValue());
+        fieldValueService.saveOrUpdate(field);
+
+    }
+
+    private void saveDoubleValue(Cell cell, int patientId, Map<Integer, String> columnIndexes) {
         FieldValue<Double> field = new DoubleFieldValue();
         field.setPatientId(patientId);
         Field fieldByName = fieldService.findByName(columnIndexes.get(cell.getColumnIndex()));
