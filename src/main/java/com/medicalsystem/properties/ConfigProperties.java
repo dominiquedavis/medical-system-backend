e67c7b91f198df6f269652a1faec0543092590f1
sienia0@gmail.com <sienia0@gmail.com>
2017-10-12 23:08:16 +0200

    Remove old properties handling

--- src/main/java/com/medicalsystem/properties/ConfigProperties.java
+++ src/main/java/com/medicalsystem/properties/ConfigProperties.java
@@ -3,6 +3,7 @@ package com.medicalsystem.properties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.List;
  * Loads automaticaly on startup.
  */
 @Component
+@PropertySource("classpath:config.properties")
 @ConfigurationProperties("")
 public class ConfigProperties {
 
