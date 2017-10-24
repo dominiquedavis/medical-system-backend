eeac7a8683ee22417b3f109808cda157b1e5f1bb
sienia0@gmail.com <sienia0@gmail.com>
2017-10-24 22:55:42 +0200

    Export controller

--- src/main/java/com/medicalsystem/controller/ExcelController.java
+++ src/main/java/com/medicalsystem/controller/ExcelController.java
@@ -1,11 +1,15 @@
 package com.medicalsystem.controller;
 
+import com.medicalsystem.excel.exporter.DataExporter;
 import com.medicalsystem.excel.importer.DataImporter;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +22,11 @@ import java.io.*;
 @Log
 public class ExcelController {
 
-    private final String FILE_NAME = "data/data.xlsx";
+    private final String IMPORT_FILE_NAME = "data/dataImport.xlsx";
+    private final String EXPORT_FILE_NAME = "data/dataExport.xlsx";
 
     private final DataImporter dataImporter;
+    private final DataExporter dataExporter;
 
     @PostMapping("api/import/excel")
     public ResponseEntity<?> importToDB(@RequestParam("file") MultipartFile file) {
@@ -28,21 +34,33 @@ public class ExcelController {
             return new ResponseEntity<>("Error uploading file", HttpStatus.BAD_REQUEST);
 
         /* Save file */
-        try (BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(FILE_NAME)))) {
+        try (BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(IMPORT_FILE_NAME)))) {
             byte[] bytes = file.getBytes();
             stream.write(bytes);
-            log.info("File uploaded: " + FILE_NAME);
+            log.info("File uploaded: " + IMPORT_FILE_NAME);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         try {
-            dataImporter.importToDatabase(new FileInputStream(FILE_NAME));
+            dataImporter.importToDatabase(new FileInputStream(IMPORT_FILE_NAME));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("api/export")
+    public ResponseEntity<?> exportToExcel() throws IOException {
+        dataExporter.exportToFile(EXPORT_FILE_NAME);
+        File file = new File(EXPORT_FILE_NAME);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        return ResponseEntity.ok()
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
     }
 
 }
--- src/main/java/com/medicalsystem/json/mapper/impl/FieldMapperImpl.java
+++ src/main/java/com/medicalsystem/json/mapper/impl/FieldMapperImpl.java
@@ -5,18 +5,49 @@ import com.medicalsystem.json.model.JSONField;
 import com.medicalsystem.model.field.Field;
 import com.medicalsystem.model.value.FieldValue;
 import com.medicalsystem.service.FieldValueService;
+import com.medicalsystem.service.PatientService;
 import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor(onConstructor = @__(@Autowired))
+@Log
 public class FieldMapperImpl implements FieldMapper {
 
     private final FieldValueService fieldValueService;
+    private final PatientService patientService;
 
     @Override
     public Field<?> fromJSON(JSONField jsonField) {
+        /*Field<?> field = fieldService.findById(jsonField.getId());
+
+        if (field == null) {
+            log.severe("Field not found with ID: " + jsonField.getId());
+            return null;
+        }
+
+        Patient patient = patientService.findById(patientId);
+
+        if (patient == null) {
+            log.severe("Patient not found with ID: " + patientId);
+            return null;
+        }
+
+        // TODO
+        switch (jsonField.getType()) {
+            case "DATE":
+                DateFieldValue fieldValue = new DateFieldValue();
+                fieldValue.setId(jsonField.getId());
+                //fieldValue.setPatient(patient);
+                fieldValue.setField(field);
+                String value = (String) jsonField.getValues().get(0);
+                fieldValue.setValue(DateUtils.fromString(value));
+                break;
+        }
+        */
+
         throw new UnsupportedOperationException("Method not implemented");
     }
 
